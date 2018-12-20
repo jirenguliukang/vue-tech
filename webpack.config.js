30048bcd4569e68fb5fc21bc7a488560ddf05f40
jirenguliukang <292582592@qq.com>
2018-12-21 00:07:47 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require ('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname,'dist')
+    },
+    plugins: [
+        // make sure to include the plugin for the magic
+        new VueLoaderPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'css-loader'
+              }
+        ]
+    }
+}
